b951f66db31326643ef4ce19eda2781ca4e9c61d
hamrain <65753207+hamrain@users.noreply.github.com>
2024-08-13 20:07:55 +0800

    Update run_non_prompt.sh

--- run_non_prompt.sh
+++ run_non_prompt.sh
@@ -2,14 +2,14 @@
 #
 NUM_GPU=1
 export OMP_NUM_THREADS=4
-export CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=0
 
 python -m torch.distributed.launch --nproc_per_node $NUM_GPU --master_port 13212 train.py \
-    --model_name_or_path ./bert-base-uncased \
+    --model_name_or_path bert-base-uncased \
     --train_file data/wiki1m_for_simcse.txt \
     --output_dir result/CMNS-nonpro-bert \
     --num_train_epochs 1\
-    --per_device_train_batch_size 128 \
+    --per_device_train_batch_size 256 \
     --save_steps 125 \
     --save_total_limit 1 \
     --learning_rate 5e-5 \
@@ -27,9 +27,8 @@ python -m torch.distributed.launch --nproc_per_node $NUM_GPU --master_port 13212
     --bml_weight 1e-4 \
     --bml_alpha 0.20 \
     --bml_beta 0.00 \
-    --guss_weight 0.127\
+    --guss_weight 0.125\
     --early_stop 6 \
-    --seed 3407 \
     --fp16 \
     "$@"
 
